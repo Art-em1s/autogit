50bb475a151d5935d5f23c22284367e24ca21cf1
Artemis <artemis@w1z0.xyz>
2023-05-06 21:38:36 +0100

    feat(autogit.py): add multithreading and time_delta function

--- autogit.py
+++ autogit.py
@@ -2,7 +2,21 @@ import requests
 import os
 from git import Repo
 import time
+from datetime import timedelta
+from concurrent.futures import ThreadPoolExecutor
 
+
+def time_delta(epoch1, epoch2):
+    delta = timedelta(seconds=epoch2 - epoch1)
+    hours, remainder = divmod(delta.seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+
+    if hours > 0:
+        return f"{hours:02d} hours, {minutes:02d} minutes, {seconds:02d} seconds"
+    elif minutes > 0:
+        return f"{minutes:02d} minutes, {seconds:02d} seconds"
+    else:
+        return f"{seconds:02d} seconds"
 
 
 def get_all_user_repos(token):
@@ -27,44 +41,59 @@ def get_all_user_repos(token):
     return repos
 
 
-def clone_or_pull_repos(token, dest_dir):
+def clone_or_pull_repo(repo, dest_dir):
+    repo_url = repo["clone_url"]
+    repo_name = repo["name"]
+    owner_name = repo["owner"]["login"]
+    owner_path = os.path.join(dest_dir, owner_name)
+    repo_path = os.path.join(owner_path, repo_name)
+
+    if not os.path.exists(owner_path):
+        os.makedirs(owner_path)
+
+    if not os.path.exists(repo_path):
+        print(f"Cloning {owner_name}/{repo_name} to {repo_path}")
+        Repo.clone_from(repo_url, repo_path)
+    else:
+        try:
+            local_repo = Repo(repo_path)
+            active_branch = local_repo.active_branch
+            local_commit = local_repo.commit(active_branch)
+            
+            remote = local_repo.remote()
+            remote.fetch()
+            remote_commit = remote.refs[active_branch.name].commit
+
+            # If the local commit is the same as the remote commit, we don't need to update
+            if local_commit.hexsha == remote_commit.hexsha:
+                print(f"Skipping {owner_name}/{repo_name}, already up to date")
+                return
+            
+            print(f"Updating {owner_name}/{repo_name} in {repo_path}")
+            local_repo.git.pull()
+        except Exception as e:
+            print(f"Error updating {owner_name}/{repo_name}: {e}")
+            return
+
+
+def spawn_threads(token, dest_dir, max_workers=5):
     start_time = time.time()
     repos = get_all_user_repos(token)
 
     if not os.path.exists(dest_dir):
         os.makedirs(dest_dir)
 
-    for repo in repos:
-        repo_url = repo["clone_url"]
-        repo_name = repo["name"]
-        owner_name = repo["owner"]["login"]
-        owner_path = os.path.join(dest_dir, owner_name)
-        repo_path = os.path.join(owner_path, repo_name)
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        tasks = [executor.submit(clone_or_pull_repo, repo, dest_dir) for repo in repos]
+        for future in tasks:
+            future.result()
 
-        if not os.path.exists(owner_path):
-            os.makedirs(owner_path)
-
-        if not os.path.exists(repo_path):
-            print(f"Cloning {repo_url} to {repo_path}")
-            Repo.clone_from(repo_url, repo_path)
-        else:
-            print(f"Updating {repo_url} in {repo_path}")
-            local_repo = Repo(repo_path)
-            try:
-                local_repo.git.pull()
-            except Exception as e:
-                print(f"Error updating {repo_name}: {e}")
-
-    end_time = time.time()
-    total_time = end_time - start_time
-    minutes, seconds = divmod(total_time, 60)
-    seconds = round(seconds, 3)
-
-    print("Execution time: {:d}:{:06.3f}".format(int(minutes), seconds))
+    print(f"Execution time: {time_delta(start_time, time.time())}")
 
 
 # Replace these with the appropriate values
 github_token = "your-personal-access-token"
 destination_directory = "path-to-destination-directory"
+max_workers = 5 # Number of threads to spawn, 10 seems to cause issues with GitHub rate limiting(?)
 
-clone_or_pull_repos(github_token, destination_directory)
+spawn_threads(github_token, destination_directory, max_workers)
